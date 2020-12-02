0794deffd3c77cd5e93fc58c3b4f9b92ad9d5ed1
Jeremy jones <j1-aggie@hotmail.com>
2020-12-02 12:37:05 -0600

    saving SQL database

---
+++ SQL_database.sql
@@ -0,0 +1,28 @@
+
+drop table if exists open_beer_database 
+
+create table open_beer_database (
+	Name VARCHAR(500),
+	id INT, 
+	brewery_id INT, 
+	cat_id INT, 
+	style_id INT, 
+	Alcohol_By_Vol INT, 
+	International_Bitterness_Units VARCHAR, 
+	Standard_Reference_Method INT, 
+	Universal_Product_Code INT, 	
+	Description VARCHAR(10000),
+	add_user INT,
+	last_mod DATE,
+	Style VARCHAR,
+	Category VARCHAR,
+	Brewer VARCHAR,
+	Address VARCHAR,
+	City VARCHAR,
+	State VARCHAR,
+	Country VARCHAR,
+	Coordinates VARCHAR
+	
+);
+
+select * from open_beer_database
